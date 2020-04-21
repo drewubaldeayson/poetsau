bf45dace1e6d715a6b944d667e240b6fb64461f3
drew <auayson@trends.com.ph>
2020-04-22 00:40:20 +0800

    adjust some images and positionings

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
-$("#year_copyright").html(new Date().getFullYear());
+let dateToday = new Date();
+const months=['','Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+const weekDays=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+const monthToday = dateToday.getMonth()+1;
+$("#year_copyright").html(dateToday.getFullYear());
+$("#dayToday").html(weekDays[dateToday.getDay()]);
+$("#dateToday").html(monthToday.length>0 ? monthToday: '0'.concat(monthToday)+"-"+dateToday.getDate()+"-"+dateToday.getFullYear());
+$("#dateTodayWithAD").html(dateToday.getDate()+" "+months[monthToday]+" "+dateToday.getFullYear()+" A.D. Earth");
