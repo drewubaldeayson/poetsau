3f9e1d01f68ae99e3320e160e33108f47f80e099
drew <auayson@trends.com.ph>
2020-04-23 01:48:52 +0800

    added search

--- js/index.js
+++ js/index.js
@@ -6,3 +6,13 @@ $("#year_copyright").html(dateToday.getFullYear());
 $("#dayToday").html(weekDays[dateToday.getDay()]);
 $("#dateToday").html(monthToday.length>0 ? monthToday: '0'.concat(monthToday)+"-"+dateToday.getDate()+"-"+dateToday.getFullYear());
 $("#dateTodayWithAD").html(dateToday.getDate()+" "+months[monthToday]+" "+dateToday.getFullYear()+" A.D. Earth");
+
+function openSearchForm_OnAnchorClick(){
+    $('body').css("overflow-y","hidden");
+    $("#search-lightbox-wrap").css("display","block");
+}
+
+function closeSearchForm_OnIconClick(){
+    $('body').css("overflow-y","visible");
+    $("#search-lightbox-wrap").css("display","none");
+}
