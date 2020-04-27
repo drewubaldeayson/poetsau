6a6a52cd2321bc7e2b34053c2b68f1b47c5527c2
drew <auayson@trends.com.ph>
2020-04-28 00:48:48 +0800

    added auto date identifier in clock

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@ let dateToday = new Date();
 const months=['','Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
 const weekDays=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 const monthToday = dateToday.getMonth()+1;
+const mainDateToday = dateToday.getDate();
+
 $("#year_copyright").html(dateToday.getFullYear());
 $("#dayToday").html(weekDays[dateToday.getDay()]);
 $("#dateToday").html(monthToday.length>0 ? monthToday: '0'.concat(monthToday)+"-"+dateToday.getDate()+"-"+dateToday.getFullYear());
@@ -10,12 +12,21 @@ $("#dateTodayWithAD").html(dateToday.getDate()+" "+months[monthToday]+" "+dateTo
 $("#year_clock").html(dateToday.getFullYear());
 
 if(monthToday.toString().length==1){
-    $("#innerWarped > .w"+(monthToday+2)).html(monthToday+`<span class="inner-clock-focus-dot"></span>`)
-}
+    $("#innerWarped > #"+monthToday+"_inner").html(monthToday+`<span class="inner-clock-focus-dot"></span>`)
+    $(".inner-clock-focus-dot").css("margin-left","3px");
+} else{
+    $("#innerWarped > #"+monthToday+"_inner").html(monthToday.toString().substring(1)+`<span class="inner-clock-focus-dot"></span>`)
+    $(".inner-clock-focus-dot").css("margin-left","-10px");
+}   
 
-
-
-
+if(mainDateToday.toString().length==1){
+    $("#warped > #"+mainDateToday+"_outer").html(mainDateToday+`<span class="clock-focus-dot"></span>`)
+    $(".clock-focus-dot").css("margin-left","2");
+} else{
+   
+    $("#warped > #"+mainDateToday+"_outer").html(mainDateToday.toString().substring(1)+`<span class="clock-focus-dot"></span>`)
+    $(".clock-focus-dot").css("margin-left","-5");
+}   
 
 function openSearchForm_OnAnchorClick(){
     $('body').css("overflow-y","hidden");
