91af512663ab12f12d1350caa4d243209c63366a
drew <auayson@trends.com.ph>
2020-04-27 12:47:03 +0800

    complete clock ad design

--- js/index.js
+++ js/index.js
@@ -7,6 +7,16 @@ $("#dayToday").html(weekDays[dateToday.getDay()]);
 $("#dateToday").html(monthToday.length>0 ? monthToday: '0'.concat(monthToday)+"-"+dateToday.getDate()+"-"+dateToday.getFullYear());
 $("#dateTodayWithAD").html(dateToday.getDate()+" "+months[monthToday]+" "+dateToday.getFullYear()+" A.D. Earth");
 
+$("#year_clock").html(dateToday.getFullYear());
+
+if(monthToday.toString().length==1){
+    $("#innerWarped > .w"+(monthToday+2)).html(monthToday+`<span class="inner-clock-focus-dot"></span>`)
+}
+
+
+
+
+
 function openSearchForm_OnAnchorClick(){
     $('body').css("overflow-y","hidden");
     $("#search-lightbox-wrap").css("display","block");
