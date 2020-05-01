dd7ebac6368caafa208deb92fc663b21727f7607
domingoysa <domingoysabelle@gmail.com>
2020-05-01 18:20:15 +0800

    fixed responsiveness

--- js/poets_coming_soon/coming_soon.js
+++ js/poets_coming_soon/coming_soon.js
@@ -1,7 +1,15 @@
 let dateToday = new Date();
-const months=['','January','February','March','April','May','June','July','August','September','October','November','December'];
-const monthToday = dateToday.getMonth()+1;
-const mainDateToday = dateToday.getDate();
-const mainYearToday = dateToday.getFullYear();
+let months=['','January','February','March','April','May','June','July','August','September','October','November','December'];
+let monthToday = dateToday.getMonth()+1;
+let mainDateToday = dateToday.getDate();
+let mainYearToday = dateToday.getFullYear();
 
-$("#date_today").html(months[monthToday]+" "+mainDateToday.toString().concat(",")+" "+mainYearToday);
+if (mainDateToday < 10) { 
+  mainDateToday = '0' + mainDateToday; 
+} 
+if (monthToday < 10) { 
+  monthToday = '0' + monthToday; 
+} 
+
+var today = mainDateToday + ' • ' + monthToday + ' • ' + mainYearToday; 
+$("#date_today").html(`${today} A.D. EARTH`);
